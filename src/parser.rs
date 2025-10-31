3a4fce85ca8ef01df10ec0f02de229556185d11f
KiFilterFiberContext <angel.j3141230@gmail.com>
2025-10-31 00:54:47 -0700

    Add check for valid register names

--- src/parser.rs
+++ src/parser.rs
@@ -135,9 +135,31 @@ impl Parser {
         }
     }
 
+    fn is_valid_register(&self, name: &str) -> bool {
+        matches!(name,
+            "$zero" | "$at" |
+            "$v0" | "$v1" |
+            "$a0" | "$a1" | "$a2" | "$a3" |
+            "$t0" | "$t1" | "$t2" | "$t3" | "$t4" | "$t5" | "$t6" | "$t7" |
+            "$s0" | "$s1" | "$s2" | "$s3" | "$s4" | "$s5" | "$s6" | "$s7" |
+            "$t8" | "$t9" |
+            "$k0" | "$k1" |
+            "$gp" | "$sp" | "$fp" | "$ra" | "$pc"
+        )
+    }
+
     fn parse_register(&mut self) -> Result<String, EmuError> {
         let token = self.expect(TokenType::RegisterName)?;
-        Ok(token.lexeme)
+        
+        if self.is_valid_register(&token.lexeme) {
+            Ok(token.lexeme)
+        } else {
+            let err_msg = format!("Line {}: Invalid Register {}", self.current_line, token.lexeme);
+            
+            self.syntax_error = true;
+            self.syntax_error_message = err_msg.clone();
+            Err(EmuError::InvalidReg(err_msg))
+        }
     }
 
     fn parse_immediate<T: std::str::FromStr>(&mut self) -> Result<T, EmuError> {
